23a51ca3afc5bbd15e433115a8e4eb7e8a99b7a9
EnriqueRS <enrqrs1@gmail.com>
2023-08-22 21:15:34 -0300

    Adicionando mapa e pacotes essenciais

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.containerMap}>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
+      <MapView style={styles.map}/>
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/* const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+}); */
+const styles = StyleSheet.create({
+  containerMap: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  });
